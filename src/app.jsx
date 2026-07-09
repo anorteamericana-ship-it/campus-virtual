e66b859440377910f84cc66effdf6bb6d6dc0cc9
anorteamericana-ship-it <anorteamericana@gmail.com>
2026-07-09 17:18:55 -0600

    Add files via upload

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
+// F98.4-Z6-CS19F · Agenda docente estable post-presentación
 // F98.4-Z6-CS19E · Agenda docente core-ready + vista semanal estable
-// F98.4-Z6-CS19A · Agenda docente slots visuales generales para todos los profes
 // F98.4-Z6-CS19D · Agenda docente modo Club I CAN aislado + slots visuales
+// F98.4-Z6-CS19A · Agenda docente slots visuales generales para todos los profes
 // F98.4-Z6-CS7B · Conexión ventas/admisiones para prematrículas
 // F98.4-Z6-CS3 · Academia Play V1.7 catálogo móvil
 // F98.4-Z6-CR · Hotfix guardado de evidencia técnica
@@ -70,22 +71,22 @@ const F96_LAZY = {
   free_user_admin: ['src/free_user_admin.jsx?v=F98.4Z6CS7B_LITE'],
   student_modules: ['src/panel_suspensiones.jsx?v=F98.4A','src/solicitudes_pago.jsx?v=F98.4A','src/solicitudes_unificadas.jsx?v=F98.4A','src/student_modules.jsx?v=F98.4Z6G'],
   syllabus_views: ['src/syllabus_views.jsx?v=F98.4Z6G'],
-  teacher_views: ['src/vista_docente.jsx?v=F98.4Z6CS19E','src/teacher_views.jsx?v=F98.4Z6CS19E','src/teacher_agenda_slots_cs19e.jsx?v=F98.4Z6CS19E'],
-  vista_docente: ['src/vista_docente.jsx?v=F98.4Z6CS19E'],
+  teacher_views: ['src/vista_docente.jsx?v=F98.4Z6CS19F','src/teacher_views.jsx?v=F98.4Z6CS19F','src/teacher_agenda_slots_cs19f.jsx?v=F98.4Z6CS19F'],
+  vista_docente: ['src/vista_docente.jsx?v=F98.4Z6CS19F'],
   admin_views: ['src/becas_admin.jsx?v=F96.5G','src/admin_views.jsx?v=F98.4Z6R'],
   admin_master: ['src/admin_master_charts.jsx?v=F98.4Z6CL','src/admin_master_dashboard.jsx?v=F98.4Z6CR'],
   admin_students: ['src/admin_students.jsx?v=F98.4Z6CD'],
   matriculas: ['src/matriculas_admin.jsx?v=F96.5G','src/matriculas_calendario.jsx?v=F96.5G','src/matriculas.jsx?v=F96.5G'],
   cronograma: ['src/cronograma.jsx?v=F96.5G'],
   cronograma_todos: ['src/cronograma_todos.jsx?v=F98.4Z6CM'],
-  cronograma_grupo: ['src/vista_docente.jsx?v=F98.4Z6CS19E','src/cronograma_todos.jsx?v=F98.4Z6CM','src/cronograma_grupo.jsx?v=F98.4Z6CM'],
-  calendario_grupo: ['src/vista_docente.jsx?v=F98.4Z6CS19E','src/cronograma_todos.jsx?v=F98.4Z6CM','src/cronograma_grupo.jsx?v=F98.4Z6CM','src/admin_students.jsx?v=F98.4Z6CD','src/calendario_grupo.jsx?v=F98.4Z6CM'],
-  docente_operativo: ['src/vista_docente.jsx?v=F98.4Z6CS19E','src/teacher_views.jsx?v=F98.4Z6CS19E','src/teacher_agenda_slots_cs19e.jsx?v=F98.4Z6CS19E','src/docente_operativo.jsx?v=F96.5G'],
+  cronograma_grupo: ['src/vista_docente.jsx?v=F98.4Z6CS19F','src/cronograma_todos.jsx?v=F98.4Z6CM','src/cronograma_grupo.jsx?v=F98.4Z6CM'],
+  calendario_grupo: ['src/vista_docente.jsx?v=F98.4Z6CS19F','src/cronograma_todos.jsx?v=F98.4Z6CM','src/cronograma_grupo.jsx?v=F98.4Z6CM','src/admin_students.jsx?v=F98.4Z6CD','src/calendario_grupo.jsx?v=F98.4Z6CM'],
+  docente_operativo: ['src/vista_docente.jsx?v=F98.4Z6CS19F','src/teacher_views.jsx?v=F98.4Z6CS19F','src/teacher_agenda_slots_cs19f.jsx?v=F98.4Z6CS19F','src/docente_operativo.jsx?v=F96.5G'],
   buscador: ['src/admin_students.jsx?v=F98.4Z6CD','src/buscador.jsx?v=F98.4Z6AS'],
   banco: ['src/importador_banco.jsx?v=F96.5G'],
   aplicar_pago: ['src/aplicar_pago.jsx?v=F98.4Z6AP'],
   conape: ['src/conape_cobranza.jsx?v=F96.5G'],
-  supervision: ['src/vista_docente.jsx?v=F98.4Z6CS19E','src/panel_admin_supervision.jsx?v=F96.5G'],
+  supervision: ['src/vista_docente.jsx?v=F98.4Z6CS19F','src/panel_admin_supervision.jsx?v=F96.5G'],
   auditoria: ['src/auditoria_academica.jsx?v=F96.5G'],
   diagnostico: ['src/diagnostico_interno.jsx?v=F98.4Z6BD'],
   permisos: ['src/permisos_roles.jsx?v=F96.5G'],
@@ -93,7 +94,7 @@ const F96_LAZY = {
   inscripcion_admin: ['src/inscripcion_admin.jsx?v=F96.5G'],
   solicitudes: ['src/panel_suspensiones.jsx?v=F98.4A','src/solicitudes_pago.jsx?v=F98.4A','src/solicitudes_unificadas.jsx?v=F98.4A'],
   student_course: [
-    'src/vista_docente.jsx?v=F98.4Z6CS19E','src/cronograma_todos.jsx?v=F98.4Z6CK','src/cronograma_grupo.jsx?v=F98.4Z6CE',
+    'src/vista_docente.jsx?v=F98.4Z6CS19F','src/cronograma_todos.jsx?v=F98.4Z6CK','src/cronograma_grupo.jsx?v=F98.4Z6CE',
     'src/syllabus_views.jsx?v=F98.4Z6G','src/student_experience.jsx?v=F98.4N'
   ],
   student_evaluations: [
